b224cf24ef6ff857fe80fd899bdd9dcad567f3b9
QZBrainon <brainonqueiroz@gmail.com>
2024-09-05 01:44:24 -0300

    separate concerns of PlayerTable component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,9 @@ import { PlayerTable } from "@/components/application/PlayerTable";
 import { useEffect, useState } from "react";
 import { showDeleteAlert } from "@/lib/utils";
 import Spinner from "@/components/application/Spinner";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+import { PlusIcon } from "lucide-react";
 
 export default function App() {
   type Player = {
@@ -39,12 +42,29 @@ export default function App() {
     await showDeleteAlert(id, fetchPlayers);
   };
 
+  const router = useRouter();
+
   return isLoading ? (
     <div className="h-full w-screen flex justify-center items-center mt-48">
       <Spinner />
     </div>
   ) : (
     <div>
+      <div className="container mx-auto w-full flex justify-between items-center py-6 gap-4 mt-28 border-b border-border/40">
+        <h1 className="text-3xl font-bold">Jogadores</h1>
+        <div className="flex justify-center items-center">
+          <Button
+            variant="default"
+            aria-label="add_player"
+            onClick={() => {
+              router.push("/jogador/novo");
+            }}
+          >
+            <PlusIcon className="mr-2 h-4 w-4" />
+            Adicionar jogador
+          </Button>
+        </div>
+      </div>
       <PlayerTable
         players={players}
         handleDeleteAndRefresh={handleDeleteAndRefresh}
--- frontend/components/application/PlayerTable.tsx
+++ frontend/components/application/PlayerTable.tsx
@@ -38,21 +38,6 @@ export function PlayerTable({
 
   return (
     <div>
-      <div className="container mx-auto w-full flex justify-between items-center py-6 gap-4 mt-28 border-b border-border/40">
-        <h1 className="text-3xl font-bold">Jogadores</h1>
-        <div className="flex justify-center items-center">
-          <Button
-            variant="default"
-            aria-label="add_player"
-            onClick={() => {
-              router.push("/jogador/novo");
-            }}
-          >
-            <PlusIcon className="mr-2 h-4 w-4" />
-            Adicionar jogador
-          </Button>
-        </div>
-      </div>
       {players.length > 0 ? (
         <div className="container mx-auto w-full flex justify-between items-center py-6 gap-4 mt-28 border-b border-border/40">
           <Table>
